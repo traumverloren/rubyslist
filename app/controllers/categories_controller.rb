1edd6cc441f7271620d835f4ff5367edaeb7768f
Stephanie Nemeth <traumverloren@gmail.com>
2014-12-31 20:29:07 +0100

    add categories to index and update cat controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,13 @@
 class CategoriesController <ApplicationController
 
   def index
+    @categories = Category.all
+    @community = @categories[0]
+    @housing = @categories[1]
+    @jobs = @categories[2]
+    @personals = @categories[3]
+    @services = @categories[4]
+    @for_sale = @categories[5]
   end
 
 end
