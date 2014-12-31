ba11fbf11a3c4995288c4e7fb5fb59cff9a4f28e
Stephanie Nemeth <traumverloren@gmail.com>
2014-12-31 17:28:17 +0100

    added static pages, added pages controller, updated routes.rb

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,24 @@
+class PagesController < ApplicationController
+
+  def help
+  end
+
+  def scams
+  end
+
+  def tips
+  end
+
+  def terms
+  end
+
+  def privacy
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
 
   root 'categories#index'
 
+  get 'help' => "pages#help"
+  get 'scams' => "pages#scams"
+  get 'tips' => "pages#tips"
+  get 'terms' => "pages#terms"
+  get 'privacy' => "pages#privacy"
+  get 'about' => "pages#about"
+  get 'contact' => "pages#contact"
+
 end
